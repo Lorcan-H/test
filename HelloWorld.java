dcc4accd508f246839b3f94bd29f6e129b479680
Lorcan-H <lorcan_healy@outlook.ie>
2016-06-02 04:05:21 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Lorca
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name1, name2, fullNme;
+        name1 = "lorcan";
+        name2 = "Healy";
+        fullNme = name1 + " " + name2;
+        
+        System.out.println(fullNme);
+        
+        
+    }
+    
+}
